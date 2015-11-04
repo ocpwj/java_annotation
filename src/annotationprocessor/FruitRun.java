029d2571aa3743a9d29836c0f3db2aaf9b0e18f9
ocpwj <ocp.wang@gmail.com>
2015-11-04 17:04:10 +0800

    Java annotation

---
+++ src/annotationprocessor/FruitRun.java
@@ -0,0 +1,15 @@
+package annotationprocessor;
+
+
+public class FruitRun {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        FruitInfoUtil.getFruitInfo(Apple.class);
+        
+    }
+
+}
